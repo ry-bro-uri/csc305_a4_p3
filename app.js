ac373226308e02570ef084fc5e57a0784fccc695
Apps816 <123339291+ry-bro-uri@users.noreply.github.com>
2024-02-19 21:46:07 -0500

    Added hook from RTDB

--- app.js
+++ app.js
@@ -4,8 +4,17 @@ var database = firebase.database();
 // Create a reference to the root of your database
 var rootRef = database.ref();
 
+// Get a reference to the 'data' div
+var dataDiv = document.getElementById('data');
+
 // Listen for data
 rootRef.once('value', function(snapshot) {
     // The snapshot argument contains the data
-    console.log(snapshot.val());
+    var data = snapshot.val();
+
+    // Convert the data to a string
+    var dataString = JSON.stringify(data, null, 2);
+
+    // Update the 'data' div
+    dataDiv.textContent = dataString;
 });
